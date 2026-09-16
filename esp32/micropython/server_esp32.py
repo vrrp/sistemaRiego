123ffc100815fa24ec7b39cacf78cab6cf2a3afa
vrrp <vr.rojaspozo@gmail.com>
2026-09-16 11:26:49 -0500

    yp

--- esp32/micropython/server_esp32.py
+++ esp32/micropython/server_esp32.py
@@ -2,16 +2,10 @@ import socket
 import network
 from utime import sleep
 
-"""
-wget http://192.168.1.45/datos_met.csv -O datos_met.csv
-crontab -e
-5 * * * * wget -q http://192.168.1.45/datos_met.csv -O /home/pi/datos_$(date +\%Y\%m\%d_\%H%M).csv
-
-
-"""
-
 WIFI_SSID = "NOMBRE_DE_TU_WIFI"
 WIFI_PASS = "CLAVE_DE_TU_WIFI"
+
+ENCABEZADO = "fecha_hora,temperatura,humedad\n"
 
 def conectar_wifi():
     wifi = network.WLAN(network.STA_IF)
@@ -22,13 +16,25 @@ def conectar_wifi():
             if wifi.isconnected():
                 break
             sleep(1)
-    print("IP del ESP32:", wifi.ifconfig()[0])   # ¡Anota esta IP!
+    print("IP del ESP32:", wifi.ifconfig()[0])
+
+def leer_y_vaciar():
+    """Lee el CSV y lo deja solo con el encabezado (buzon vacio)."""
+    try:
+        with open("datos_met.csv", "r") as f:
+            datos = f.read()
+    except OSError:
+        datos = ENCABEZADO
+    # Vaciar el buzon: dejar solo el encabezado para los datos nuevos
+    with open("datos_met.csv", "w") as f:
+        f.write(ENCABEZADO)
+    return datos
 
 conectar_wifi()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("0.0.0.0", 80))    # taquilla en el puerto 80
+s.bind(("0.0.0.0", 80))
 s.listen(1)
 print("Servidor listo, esperando pedidos...")
 
@@ -36,12 +42,12 @@ while True:
     conn, direccion = s.accept()
     print("Pedido de:", direccion)
     try:
-        conn.recv(512)   # leer el pedido HTTP (no nos importa qué pidieron)
-        with open("datos_met.csv") as f:
-            datos = f.read()
+        conn.recv(512)
+        datos = leer_y_vaciar()          # entregar Y vaciar en el mismo acto
         conn.send("HTTP/1.0 200 OK\r\nContent-Type: text/csv\r\n\r\n")
         conn.sendall(datos.encode())
+        print("Enviados {} bytes. Buzon vaciado.".format(len(datos)))
     except OSError:
-        pass
+        print("Fallo al enviar (el buzon NO se vacio)")
     conn.close()
 
